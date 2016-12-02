4564d88d428a4931092100ddcfe4855de1d38ccf
kozharsky <dmitriy.kozharsky@gmail.com>
2016-12-02 12:48:11 +0200

    wordpress

---
+++ docker-images/build-and-deploy.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+set -e
+
+for i in "$@"
+do
+case $i in
+    --stack-name=*)
+    STACK_NAME=`echo $i | sed 's/[-a-zA-Z0-9]*=//'`
+    ;;
+    --pod=*)
+    POD=`echo $i | sed 's/[-a-zA-Z0-9]*=//'`
+    ;;
+    --version=*)
+    VERSION=`echo $i | sed 's/[-a-zA-Z0-9]*=//'`
+    ;;
+    *)
+    echo "Unknown option ${i}"
+    ;;
+esac
+done
+
+eval $(aws ecr get-login --region ${REGION})
+
+echo "Updating ${POD} $STACK_NAME, builded version ${VERSION}"
+docker build -t ${POD}:${VERSION} ./docker-images/${POD}
+
+ECR_TAG="${ECR_REPO}/${POD}:${VERSION}"
+
+docker tag "${POD}:${VERSION}" "${ECR_TAG}"
+docker push "${ECR_TAG}"
+echo "ECR = ${ECR_TAG}"
+/usr/local/bin/kubectl set image deployment/${STACK_NAME}-${POD} ${STACK_NAME}-${POD}=${ECR_TAG}
+echo "/usr/local/bin/kubectl set image deployment/${STACK_NAME}-${POD} ${STACK_NAME}-${POD}=${ECR_TAG}"
